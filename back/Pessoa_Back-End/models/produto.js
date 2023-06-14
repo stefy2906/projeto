a3f56a755d7eb12c52368dc584753d2a1b1eb7ec
stefy2906 <stefanybreve@gmail.com>
2023-06-14 15:55:06 -0400

    implementação do modelo

--- back/Pessoa_Back-End/models/produto.js
+++ back/Pessoa_Back-End/models/produto.js
@@ -15,7 +15,7 @@ class Produto {
     try {
       const connect = await db.connect();
       const sql = "SELECT *FROM produtos WHERE id=$1";
-      return await connect.query(sql,[id]);
+      return await connect.query(sql,[id]); //usa o $ para segurançae e substitui valores
     } catch (error) {
       console.error('Erro em select:', error);
       throw error;
@@ -37,7 +37,7 @@ class Produto {
   static async update(id, data) {
     try {
       const connect = await db.connect();
-      const sql = "";
+      const sql = "UPDATE produtos SET nome=$1, data_cadastro=$2, preco=$3, descricao=$4, imagem=$5 WHERE id=$6 RETURNING id, nome, data_cadastro, preco, descricao, imagem;";
       const values = [data.nome, data.data_cadastro, data.preco, data.descricao, data.imagem, id];
       return await connect.query(sql, values);
     } catch (error) {
@@ -49,7 +49,7 @@ class Produto {
   static async delete(id) {
     try {
       const connect = await db.connect();
-      const sql = "DELETE FROM produtos WHERE id=$1";
+      const sql = "DELETE FROM produtos WHERE id=$1 RETURNING id, nome, data_cadastro, preco, descricao, imagem;";
       return await connect.query(sql, [id]);
     } catch (error) {
       console.error('Erro em delete:', error);
